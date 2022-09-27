53b9f224baf129883f9c9a6a1111c121a4952659
Taras <tarasmikulskii@ukr.net>
2022-09-27 12:06:55 +0300

    An adaptive layout for the main page has been created, a shopping cart page has been created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,11 @@ function Home() {
                 setPizzas(json);
                 setIsLoading(false);
             });
+        window.scrollTo(0, 0);
     }, []);
 
     return(
-        <>
+        <div className="container">
             <div className="content__top">
                 <Categories />
                 <Sort />
@@ -32,7 +33,7 @@ function Home() {
                         : pizzas.map(value => <PizzaBlock key={value.id} {...value} />)
                 }
             </div>
-        </>
+        </div>
     );
 }
 
